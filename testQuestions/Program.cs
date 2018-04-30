23fa5ab0bf863cde5788b30ce7d0c2f53fef0ab2
mattjopete <matthewpeterein@gmail.com>
2018-04-30 17:06:04 -0500

    Project scaffolding

---
+++ testQuestions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
